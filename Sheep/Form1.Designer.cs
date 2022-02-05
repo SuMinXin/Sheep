31a0700a9665da969fca22a44bf92397ccfb7419
MinxinSU <sian415@gmail.com>
2022-02-06 00:36:20 +0800

    for fun

---
+++ Sheep/Form1.Designer.cs
@@ -0,0 +1,136 @@
+﻿
+namespace Sheep
+{
+    partial class Form1
+    {
+        /// <summary>
+        ///  Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        ///  Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        ///  Required method for Designer support - do not modify
+        ///  the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.btnStart = new System.Windows.Forms.Button();
+            this.btn1 = new System.Windows.Forms.Button();
+            this.btn2 = new System.Windows.Forms.Button();
+            this.btn3 = new System.Windows.Forms.Button();
+            this.btnRestart = new System.Windows.Forms.Button();
+            this.pnlContent = new System.Windows.Forms.FlowLayoutPanel();
+            this.SuspendLayout();
+            // 
+            // btnStart
+            // 
+            this.btnStart.BackColor = System.Drawing.SystemColors.AppWorkspace;
+            this.btnStart.Location = new System.Drawing.Point(12, 12);
+            this.btnStart.Name = "btnStart";
+            this.btnStart.Size = new System.Drawing.Size(75, 23);
+            this.btnStart.TabIndex = 0;
+            this.btnStart.Text = "Start";
+            this.btnStart.UseVisualStyleBackColor = false;
+            this.btnStart.Click += new System.EventHandler(this.btnStart_Click);
+            // 
+            // btn1
+            // 
+            this.btn1.BackColor = System.Drawing.SystemColors.AppWorkspace;
+            this.btn1.Location = new System.Drawing.Point(13, 12);
+            this.btn1.Name = "btn1";
+            this.btn1.Size = new System.Drawing.Size(47, 23);
+            this.btn1.TabIndex = 2;
+            this.btn1.Text = "1";
+            this.btn1.UseVisualStyleBackColor = false;
+            this.btn1.Visible = false;
+            this.btn1.Click += new System.EventHandler(this.btn1_Click);
+            // 
+            // btn2
+            // 
+            this.btn2.BackColor = System.Drawing.SystemColors.AppWorkspace;
+            this.btn2.Location = new System.Drawing.Point(75, 12);
+            this.btn2.Name = "btn2";
+            this.btn2.Size = new System.Drawing.Size(47, 23);
+            this.btn2.TabIndex = 3;
+            this.btn2.Text = "2";
+            this.btn2.UseVisualStyleBackColor = false;
+            this.btn2.Visible = false;
+            this.btn2.Click += new System.EventHandler(this.btn2_Click);
+            // 
+            // btn3
+            // 
+            this.btn3.BackColor = System.Drawing.SystemColors.AppWorkspace;
+            this.btn3.Location = new System.Drawing.Point(137, 12);
+            this.btn3.Name = "btn3";
+            this.btn3.Size = new System.Drawing.Size(47, 23);
+            this.btn3.TabIndex = 4;
+            this.btn3.Text = "3";
+            this.btn3.UseVisualStyleBackColor = false;
+            this.btn3.Visible = false;
+            this.btn3.Click += new System.EventHandler(this.btn3_Click);
+            // 
+            // btnRestart
+            // 
+            this.btnRestart.BackColor = System.Drawing.SystemColors.AppWorkspace;
+            this.btnRestart.Location = new System.Drawing.Point(12, 12);
+            this.btnRestart.Name = "btnRestart";
+            this.btnRestart.Size = new System.Drawing.Size(75, 23);
+            this.btnRestart.TabIndex = 5;
+            this.btnRestart.Text = "ReStart";
+            this.btnRestart.UseVisualStyleBackColor = false;
+            this.btnRestart.Visible = false;
+            this.btnRestart.Click += new System.EventHandler(this.btnRestart_Click);
+            // 
+            // pnlContent
+            // 
+            this.pnlContent.Location = new System.Drawing.Point(13, 41);
+            this.pnlContent.Name = "pnlContent";
+            this.pnlContent.Size = new System.Drawing.Size(517, 348);
+            this.pnlContent.TabIndex = 6;
+            this.pnlContent.Paint += new System.Windows.Forms.PaintEventHandler(this.pnlContent_Paint);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(7F, 15F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.SystemColors.ActiveCaptionText;
+            this.ClientSize = new System.Drawing.Size(548, 405);
+            this.Controls.Add(this.pnlContent);
+            this.Controls.Add(this.btnRestart);
+            this.Controls.Add(this.btn3);
+            this.Controls.Add(this.btn2);
+            this.Controls.Add(this.btn1);
+            this.Controls.Add(this.btnStart);
+            this.ForeColor = System.Drawing.SystemColors.ControlText;
+            this.Name = "Form1";
+            this.Text = "賭徒本性";
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button btnStart;
+        private System.Windows.Forms.Button btn1;
+        private System.Windows.Forms.Button btn2;
+        private System.Windows.Forms.Button btn3;
+        private System.Windows.Forms.Button btnRestart;
+        private System.Windows.Forms.FlowLayoutPanel pnlContent;
+    }
+}
+
---
+++ Sheep/Form1.cs
@@ -0,0 +1,137 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Threading;
+using System.Windows.Forms;
+
+namespace Sheep
+{
+    public partial class Form1 : Form
+    {
+        public static string choosen { get; set; }
+        public static string open { get; set; }
+        public static string option { get; set; }
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void btnStart_Click(object sender, EventArgs e)
+        {
+            btnStart.Hide();
+            btn1.Show();
+            btn2.Show();
+            btn3.Show();
+
+            List<string> options = new List<string>() { "1", "2", "3" };
+            //set choosen
+            int index = randomList(options);
+            choosen = options[index];
+            options.RemoveAt(index);
+            // set open
+            index = randomList(options);
+            open = options[index];
+            addLabel("Hi, 從1-3道門中選擇一扇吧?");
+        }
+
+        private void addLabel(string value) {
+            Label lbl = new Label();
+            lbl.Text = value;
+            lbl.ForeColor = System.Drawing.Color.Orange;
+            lbl.Width = lbl.PreferredWidth;
+            pnlContent.Controls.Add(lbl);
+            Label newline = new Label();
+            newline.Text = "<br/>";
+            newline.Width = pnlContent.Width;
+            pnlContent.Controls.Add(newline);
+            this.Refresh(); //即時更新
+        }
+
+        private int randomList<T>(List<T> list) {
+            var random = new Random();
+            return random.Next(list.Count);
+        }
+
+        private void btn1_Click(object sender, EventArgs e)
+        {
+            if (string.IsNullOrEmpty(option)) {
+                openDoor(btn1.Text);
+            }
+            else 
+            {
+                showEnd(btn1.Text);
+            }
+        }
+
+        private void btn2_Click(object sender, EventArgs e)
+        {
+            if (string.IsNullOrEmpty(option))
+            {
+                openDoor(btn2.Text);
+            }
+            else
+            {
+                showEnd(btn2.Text);
+            }
+        }
+
+        private void btn3_Click(object sender, EventArgs e)
+        {
+            if (string.IsNullOrEmpty(option))
+            {
+                openDoor(btn3.Text);
+            }
+            else
+            {
+                showEnd(btn3.Text);
+            }
+        }
+
+        private void openDoor(string door) {
+            option = door;
+            Thread.Sleep(200);
+            addLabel("噢, 選定了" + door + "號門嗎?");
+            Thread.Sleep(1000);
+            addLabel("Hmmm... 要不要再想想?");
+            Thread.Sleep(2000);
+            addLabel("(推開" + open + "號門, 門後空空如也)");
+            Thread.Sleep(1000);
+            foreach (Control item in this.Controls) {
+                if (item.Name.Equals("btn" + open)) {
+                    item.Visible = false;
+                }
+            }
+            addLabel("再給你一次機會w");
+            pnlContent.Focus();
+        }
+
+        private void showEnd(string door)
+        {
+            if (door.Equals(choosen))
+            {
+                addLabel("啊哈! 就是它沒錯");
+            }
+            else {
+                addLabel("噢...不是讓你重選了嗎...");
+            }
+            btn1.Visible = false;
+            btn2.Visible = false;
+            btn3.Visible = false;
+            btnRestart.Visible = true;
+        }
+
+        private void btnRestart_Click(object sender, EventArgs e)
+        {
+            btnRestart.Visible = false;
+            btnStart.Visible = true;
+            choosen = string.Empty;
+            open = string.Empty;
+            option = string.Empty;
+            pnlContent.Controls.Clear();
+        }
+
+        private void pnlContent_Paint(object sender, PaintEventArgs e)
+        {
+
+        }
+    }
+}
